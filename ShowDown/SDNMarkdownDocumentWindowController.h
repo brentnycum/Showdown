c4d562958b6d22df396c0fad718989ed2d3d41f8
Brent Nycum <brentnycum@gmail.com>
2014-03-06 22:26:00 -0600

    SDNMarkdownDocumentWindowController

---
+++ ShowDown/SDNMarkdownDocumentWindowController.h
@@ -0,0 +1,15 @@
+//
+//  SDNMarkdownDocumentWindowController.h
+//  ShowDown
+//
+//  Created by Brent Nycum on 3/6/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SDNMarkdownDocumentWindowController : NSWindowController
+
+
+
+@end
---
+++ ShowDown/SDNMarkdownDocumentWindowController.m
@@ -0,0 +1,20 @@
+//
+//  SDNMarkdownDocumentWindowController.m
+//  ShowDown
+//
+//  Created by Brent Nycum on 3/6/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import <WebKit/WebKit.h>
+#import "SDNMarkdownDocumentWindowController.h"
+
+@implementation SDNMarkdownDocumentWindowController
+
+#pragma mark - NSWindowController
+
+- (void)windowDidLoad {
+	[super windowDidLoad];
+}
+
+@end

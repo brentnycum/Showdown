4430f468078bfb9590e043727887c54ea0a6e3d3
Brent Nycum <brentnycum@gmail.com>
2014-03-06 21:45:56 -0600

    Removing things.

--- ShowDown/SDNAppDelegate.h
+++ ShowDown/SDNAppDelegate.h
@@ -10,6 +10,4 @@
 
 @interface SDNAppDelegate : NSObject <NSApplicationDelegate>
 
-@property (assign) IBOutlet NSWindow *window;
-
 @end
--- ShowDown/SDNAppDelegate.m
+++ ShowDown/SDNAppDelegate.m
@@ -10,9 +10,8 @@
 
 @implementation SDNAppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-	// Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	
 }
 
 @end

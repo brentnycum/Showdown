d39cab18febe266d182162028e4620153a63e9a7
Brent Nycum <brentnycum@gmail.com>
2014-03-06 21:58:24 -0600

    Start of FSEvents.

--- ShowDown/SDNMarkdownDocument.m
+++ ShowDown/SDNMarkdownDocument.m
@@ -10,4 +10,41 @@
 
 @implementation SDNMarkdownDocument
 
+#pragma mark - FSEvents
+
+void fileChangedCallback(ConstFSEventStreamRef streamRef,
+				void *clientCallBackInfo,
+				size_t numEvents,
+				void *eventPaths,
+				const FSEventStreamEventFlags eventFlags[],
+				const FSEventStreamEventId eventIds[]) {
+	
+	SDNMarkdownDocument *document = (__bridge SDNMarkdownDocument *)(clientCallBackInfo);
+	
+}
+
+#pragma mark - NSDocument
+
+- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
+	CFStringRef pathRef = (__bridge CFStringRef)[self.fileURL path];
+	
+    CFArrayRef pathsToWatch = CFArrayCreate(NULL, (const void **)&pathRef, 1, NULL);
+	FSEventStreamContext context;
+    memset(&context, 0, sizeof(context));
+	context.info = (__bridge void *)(self);
+	
+    FSEventStreamRef stream = FSEventStreamCreate(NULL,
+								 &fileChangedCallback,
+								 &context,
+								 pathsToWatch,
+								 kFSEventStreamEventIdSinceNow,
+								 1.0,
+								 kFSEventStreamCreateFlagFileEvents);
+	
+	FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+	FSEventStreamStart(stream);
+	
+	return YES;
+}
+
 @end

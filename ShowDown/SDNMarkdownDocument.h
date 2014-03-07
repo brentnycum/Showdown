9d1954329e599dfa19eb5c6f99b60d509d0f7851
Brent Nycum <brentnycum@gmail.com>
2014-03-06 21:50:42 -0600

    SDNMarkdownDocument

---
+++ ShowDown/SDNMarkdownDocument.h
@@ -0,0 +1,13 @@
+//
+//  SDNMarkdownDocument.h
+//  ShowDown
+//
+//  Created by Brent Nycum on 3/6/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SDNMarkdownDocument : NSDocument
+
+@end
---
+++ ShowDown/SDNMarkdownDocument.m
@@ -0,0 +1,13 @@
+//
+//  SDNMarkdownDocument.m
+//  ShowDown
+//
+//  Created by Brent Nycum on 3/6/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import "SDNMarkdownDocument.h"
+
+@implementation SDNMarkdownDocument
+
+@end

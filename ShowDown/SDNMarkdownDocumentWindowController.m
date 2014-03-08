d02dfce8a80ac555f0812041d03aa440bdadc0a8
Brent Nycum <brentnycum@gmail.com>
2014-03-08 13:49:26 -0600

    Use proper HTML

--- ShowDown/SDNMarkdownDocumentWindowController.m
+++ ShowDown/SDNMarkdownDocumentWindowController.m
@@ -24,7 +24,18 @@
 - (void)reloadWebView {
 	SDNMarkdownDocument *markdownDocument = (SDNMarkdownDocument *)self.document;
 	
-	[self.webView.mainFrame loadHTMLString:[markdownDocument markdownRepresentation] baseURL:nil];
+	NSString *htmlRepresentation = [NSString stringWithFormat:@"<!doctype html>\
+									<html lang='en'>\
+									<head>\
+									<meta charset='UTF-8'>\
+									<title>ShowDown</title>\
+									</head>\
+									<body>\
+									%@\
+									</body>\
+									</html>", [markdownDocument markdownRepresentation]];
+	
+	[self.webView.mainFrame loadHTMLString:htmlRepresentation baseURL:nil];
 }
 
 @end

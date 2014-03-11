2a6fc918e0e0beea0a33c9b94e527f3ccbb7dddc
Brent Nycum <brentnycum@gmail.com>
2014-03-10 22:24:16 -0500

    Starting Color Scheme switcher

--- ShowDown/SDNAppDelegate.m
+++ ShowDown/SDNAppDelegate.m
@@ -8,9 +8,28 @@
 
 #import "SDNAppDelegate.h"
 
+@interface SDNAppDelegate (private)
+
+- (void)_buildColorSchemeMenu;
+- (void)_switchColorScheme:(id)sender;
+
+@end
+
 @implementation SDNAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	[self _buildColorSchemeMenu];
+}
+
+- (void)_buildColorSchemeMenu {
+	[_colorSchemeMenu removeAllItems];
+	
+	NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:@"Bootstrap" action:@selector(_switchColorScheme:) keyEquivalent:@""];
+	
+	[_colorSchemeMenu addItem:menuItem];
+}
+
+- (void)_switchColorScheme:(id)sender {
 	
 }
 

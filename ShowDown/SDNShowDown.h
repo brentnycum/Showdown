b397ef019e20545d563b132d2698ddcef31e0612
Brent Nycum <brentnycum@gmail.com>
2014-03-10 22:45:49 -0500

    Create global class.

---
+++ ShowDown/SDNShowDown.h
@@ -0,0 +1,13 @@
+//
+//  SDNShowDown.h
+//  ShowDown
+//
+//  Created by Brent Nycum on 3/10/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDNShowDown : NSObject
+
+@end
---
+++ ShowDown/SDNShowDown.m
@@ -0,0 +1,13 @@
+//
+//  SDNShowDown.m
+//  ShowDown
+//
+//  Created by Brent Nycum on 3/10/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import "SDNShowDown.h"
+
+@implementation SDNShowDown
+
+@end
